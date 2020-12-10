b7fd372cd5675fd31281f37f45c5a74e3c68179d
Suryansh555 <suryansh555@yahoo.in>
2020-12-10 15:07:14 +0530

    More Problems Solved

---
+++ Selectionsort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int n ;
+	cin >> n ;
+	int arr[n];
+	for(int a = 0 ; a < n ; a++){
+		cin >> arr[a];
+	}
+	int i, j, min_idx; 
+  
+    // One by one move boundary of unsorted subarray 
+    for (i = 0; i < n-1; i++) 
+    { 
+        // Find the minimum element in unsorted array 
+        min_idx = i; 
+        for (j = i+1; j < n; j++) 
+          if (arr[j] < arr[min_idx]) 
+            min_idx = j; 
+  
+        // Swap the found minimum element with the first element 
+        swap(arr[min_idx], arr[i]); 
+	}
+	for(int z = 0 ; z < n ; z++){
+
+		cout<< arr[z] << endl;
+	} 
+
+	return 0;
+}
---
+++ Sort 0 and 1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main() {
+int a;
+	cin >> a;
+	int arr[a];
+	for(int i = 0 ; i < a ; i++){
+		cin >> arr[i];
+		int key = arr[i];
+		int j = i - 1;
+		while(j >= 0 && arr[j] > key){
+			arr[j+1] = arr[j];
+			j = j - 1;
+		}
+		arr[j+1] = key ;
+	}
+	for(int b = 0 ; b < a ; b++){
+		cout<<arr[b]<<" ";
+	}
+	return 0;
+}
