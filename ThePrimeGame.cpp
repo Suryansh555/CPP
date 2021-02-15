66db6e1b7b059f6778c80348a2bf14dc5d292f62
Suryansh555 <suryansh555@yahoo.in>
2021-02-16 03:25:32 +0530

    More Problems

---
+++ ThePrimeGame.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(int A, int B) {
+    if(B == 1){
+        return 2 ;
+    }
+    if(A % 2 == 0)
+        return 2 ;
+    return 1 ;    
+}
+
+int main(){
+
+
+
+    return 0 ;
+}
